587b91ae27100579b808474772175b2f282ce8f0
Estevo U. Caamaño Castro <euccastro@gmail.com>
2020-12-19 12:07:13 +0100

    day 19 part 2

--- src/advent/day19.clj
+++ src/advent/day19.clj
@@ -70,3 +70,115 @@ aaaabbb")
 (def real-input (slurp (io/resource "input19")))
 
 (count-matches real-input)
+
+
+;;; for part 2 I give up on regexes
+
+(def demo-input2 "42: 9 14 | 10 1
+9: 14 27 | 1 26
+10: 23 14 | 28 1
+1: \"a\"
+11: 42 31
+5: 1 14 | 15 1
+19: 14 1 | 14 14
+12: 24 14 | 19 1
+16: 15 1 | 14 14
+31: 14 17 | 1 13
+6: 14 14 | 1 14
+2: 1 24 | 14 4
+0: 8 11
+13: 14 3 | 1 12
+15: 1 | 14
+17: 14 2 | 1 7
+23: 25 1 | 22 14
+28: 16 1
+4: 1 1
+20: 14 14 | 1 15
+3: 5 14 | 16 1
+27: 1 6 | 14 18
+14: \"b\"
+21: 14 1 | 1 14
+25: 1 1 | 1 14
+22: 14 14
+8: 42
+26: 14 22 | 1 20
+18: 15 15
+7: 14 5 | 1 21
+24: 14 1
+
+abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+bbabbbbaabaabba
+babbbbaabbbbbabbbbbbaabaaabaaa
+aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+bbbbbbbaaaabbbbaaabbabaaa
+bbbababbbbaaaaaaaabbababaaababaabab
+ababaaaaaabaaab
+ababaaaaabbbaba
+baabbaaaabbaaaababbaababb
+abbbbabbbbaaaababbbbbbaaaababb
+aaaaabbaabaaaaababaa
+aaaabbaaaabbaaa
+aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+babaaabbbaaabaababbaabababaaab
+aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba")
+
+(defn compile-rule [xs]
+  (cond
+
+    (string? (first xs)) [:char (ffirst xs)]
+
+    (some #{'|} xs) (let [[a [_ & b]]
+                          (split-with (complement #{'|}) xs)]
+                      [:or (compile-rule a) (compile-rule b)])
+    :else [:cat xs]))
+
+
+(defmulti match (fn [[rule-type _] & _]
+                  rule-type))
+
+
+(defmethod match :char
+  [[_ char] _ s]
+  (when (= char (first s))
+    (list (subs s 1))))
+
+
+(defmethod match :or
+  [[_ a b] rules s]
+  (lazy-cat (match a rules s) (match b rules s)))
+
+
+(defmethod match :cat
+  [[_ indices] rules s]
+  (if (empty? indices)
+    (list s)
+    (let [[m & alts]
+          (match (rules (first indices)) rules s)
+          match-rest #(match [:cat (rest indices)] rules %) ]
+      (cond->> (mapcat match-rest alts)
+        m (concat (match-rest m))))))
+
+
+(defmethod match :default
+  [& args]
+  (throw (ex-info "wat" args)))
+
+
+(defn matches? [rules s]
+  (some #{""} (match (rules 0) rules s)))
+
+
+(defn matching-strings [input]
+  (let [[patterns strings] (map str/split-lines (str/split input #"\R\R"))
+        patterns (merge (into {} (map parse-pattern patterns))
+                        {8 '(42 | 42 8)
+                         11 '(42 31 | 42 11 31)})
+        rules (into {} (map (fn [[idx pat]] [idx (compile-rule pat)])
+                            patterns))]
+    (filter (partial matches? rules) strings)))
+
+(count (matching-strings demo-input2))
+;; => 12
+
+(time (count (matching-strings real-input)))
+;; => 309
