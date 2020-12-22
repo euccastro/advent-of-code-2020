954e3ffe913c17772c4ab300659c4b1c58d5dbb5
Estevo U. Caamaño Castro <euccastro@gmail.com>
2020-12-22 07:33:09 +0100

    wip

--- src/advent/day22.clj
+++ src/advent/day22.clj
@@ -5,6 +5,15 @@
 
 (def input (slurp (io/resource "input22")))
 
+(def input "Player 1:
+43
+19
+
+Player 2:
+2
+29
+14")
+
 (defn parse-long [x]
   (Long/parseLong x))
 
@@ -15,24 +24,45 @@
                    rest                 ; skip player number
                    (map parse-long)))))
 
-(defn turn [decks]
-  (let [[[a & as] [b & bs]] decks]
-    (assert (not= a b))
-    (if (< a b)
-      [as (concat bs (list b a))]
-      [(concat as (list a b)) bs])))
+(defn nonrecursive-turn [[[a & as] [b & bs]]]
+  (assert (not= a b))
+  (if (< a b)
+    [as (concat bs (list b a))]
+    [(concat as (list a b)) bs]))
 
-(defn solution1 [input]
-  (->> input
-       initial-decks
-       (iterate turn)
+(defn play-nonrecursive-combat [decks]
+  (->> decks
+       (iterate nonrecursive-turn)
        (drop-while (partial every? seq))
-       first
-       (filter seq)
-       first
+       first))
+
+(defn score [result]
+  (->> result
        reverse
        (map * (iterate inc 1))
        (reduce +)))
 
+(defn solution1 [input]
+  (->> input
+       initial-decks
+       play-nonrecursive-combat
+       (filter seq)
+       first
+       score))
+
 (solution1 input)
 ;; => 33680
+
+(defn recursive-turn [seen
+                      [[a & as :as a-deck]
+                       [b & bs :as b-deck]
+                       :as decks]]
+  (cond
+    (seen decks)
+    [:a a-deck]
+
+    (every? #(>= (first %) (count (rest %))) decks)
+    ()
+    (recursive-turn seen as bs)
+    )
+  )
