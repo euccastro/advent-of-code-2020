8d89563a8909dc1865c2108470b4a928a3394dd1
Estevo U. Caamaño Castro <euccastro@gmail.com>
2020-12-24 01:30:01 +0100

    day 23, part 2, linked list based alternative

--- src/advent/day23.clj
+++ src/advent/day23.clj
@@ -80,24 +80,54 @@
     (aset a (long (dec cs)) current)))
 
 ;; takes 33 minutes :P
-(time
- (let [array (int-array circle-size (initial-circle2 real-input))
-       _ (doseq [_ (range 10000000)]
-           (step2 array))
-       i1 (Ints/indexOf array 1)]
-   (* (aget array (inc i1))
-      (aget array (inc (inc i1))))))
+(comment
+  (time
+   (let [array (int-array circle-size (initial-circle2 real-input))
+         _ (doseq [_ (range 10000000)]
+             (step2 array))
+         i1 (Ints/indexOf array 1)]
+     (* (aget array (inc i1))
+        (aget array (inc (inc i1)))))))
 ;; => 44541319250
 
 
+;;; part 2, linked-list based alternative
+;;;
+;;; state is represented as an array where the first element is the current
+;;; cup, and the element at index i contains the next cup from i in the circle.
+
+(defn build-linked-list [input]
+  (let [a (int-array (inc circle-size))
+        cups (initial-circle2 input)]
+    (aset-int a 0 (first cups))
+    (aset-int a circle-size (first cups))
+    (doseq [[cup next-cup] (partition 2 1 cups)]
+      (aset-int a cup next-cup))
+    a))
+
+(defn step2-ll [^ints a]
+  (let [[current p1 p2 p3] (take 4 (rest (iterate #(aget a %) 0)))
+        dest (pick-dest (dec current) p1 p2 p3)
+        p3-val (aget a p3)
+        dest-val (aget a dest)]
+    (aset-int a 0 p3-val)
+    (aset-int a current p3-val)
+    (aset-int a dest p1)
+    (aset-int a p3 dest-val)))
+
+(defn solution2-ll [input]
+  (let [arr (build-linked-list input)]
+    (dotimes [_ 10000000]
+      (step2-ll arr))
+    (let [next-to-one (aget arr 1)
+          second-next-to-one (aget arr next-to-one)]
+      (* next-to-one second-next-to-one))))
 
 
 (comment
-  (let [arr (int-array circle-size (initial-circle2 demo-input))]
-    (print
-     (tufte/format-pstats
-      (second (profiled
-               {}
-               (time (doseq [_ (range 100000)]
-                       (step2 arr))))))))
+
+  ;; runs in ~10sec
+  (time (solution2-ll real-input))
+;; => 44541319250
+
   )
