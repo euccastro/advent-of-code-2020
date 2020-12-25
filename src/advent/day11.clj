a217ba3d47cb6c73e73580c040dbb11a48d90554
Estevo U. C. Castro <euccastro@gmail.com>
2020-12-25 19:24:17 +0100

    day 11 part 2

--- src/advent/day11.clj
+++ src/advent/day11.clj
@@ -15,9 +15,31 @@ LLLLLLLLLL
 L.LLLLLL.L
 L.LLLLL.LL")
 
+(def demo-input2 ".......#.
+...#.....
+.#.......
+.........
+..#L....#
+....#....
+.........
+#........
+...#.....")
+
+(def demo-input3 ".............
+.L.L.#.#.#.#.
+.............")
+
+(def demo-input4 ".##.##.
+#.#.#.#
+##...##
+...L...
+##...##
+#.#.#.#
+.##.##.")
+
 (def real-input (slurp (io/resource "input11")))
 
-(def input demo-input)
+(def input real-input)
 
 (def waiting-area
   (->> input
@@ -55,9 +77,61 @@ L.LLLLL.LL")
         row)))
     wa)))
 
-(->> waiting-area
-     (util/fixed-point step)
-     flatten
-     (filter #{\#})
-     count)
+(defn solution [step-fn]
+  (->> waiting-area
+       (util/fixed-point step-fn)
+       flatten
+       (filter #{\#})
+       count))
+
+;(solution step)
 ;; => 2243
+
+
+;;; part 2
+
+
+(def directions
+  (remove
+   #{[0 0]}
+   (combo/cartesian-product
+    [-1 0 1]
+    [-1 0 1])))
+
+(defn find-seat [wa pos dir]
+  (->> pos
+       (iterate (partial map + dir))
+       rest
+       (drop-while #(= (get-in wa (reverse %)) \.))
+       first
+       reverse
+       (get-in wa)
+       (= \#)))
+
+(defn occupied-visible-seat-count [wa coords]
+  (count
+   (filter #(find-seat wa coords %)
+           directions)))
+
+(defn step2 [wa]
+  (vec
+   (map-indexed
+    (fn [y row]
+      (vec
+       (map-indexed
+        (fn [x pos]
+          (cond
+            (and (= pos \L)
+                 (zero? (occupied-visible-seat-count wa [x y])))
+            \#
+
+            (and (= pos \#)
+                 (<= 5 (occupied-visible-seat-count wa [x y])))
+            \L
+
+            :else pos))
+        row)))
+    wa)))
+
+(solution step2)
+;; => 2027
