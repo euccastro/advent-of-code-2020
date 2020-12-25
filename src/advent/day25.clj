221422bd806252e9a44b2bdacfaa99fe3c4ace26
Estevo U. C. Castro <euccastro@gmail.com>
2020-12-25 06:38:21 +0100

    day 25 part 1

---
+++ src/advent/day25.clj
@@ -0,0 +1,36 @@
+(ns advent.day25
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]))
+
+(def input
+  (-> (io/resource "input25")
+      slurp
+      str/split-lines
+      (->> (map read-string))))
+
+(defn transform-step [subject-number n]
+  (rem (* n subject-number) 20201227))
+
+(defn transform [subject-number loop-size]
+  (nth
+   (iterate (partial transform-step subject-number) 1)
+   loop-size))
+
+(defn crack [target-number]
+  (first
+   (keep-indexed
+    (fn [idx n]
+      (when (= n target-number)
+        idx))
+    (iterate (partial transform-step 7) 1))))
+
+(crack 5764801)
+;; => 8
+
+(crack 17807724)
+;; => 11
+
+(let [[pk1 _] input
+      [_ ls2] (map crack input)]
+  (transform pk1 ls2))
+;; => 1478097
