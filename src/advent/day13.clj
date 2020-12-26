515182f6f440e8bb28561c598f8cf65a0e54fb9d
Estevo U. C. Castro <euccastro@gmail.com>
2020-12-26 02:12:58 +0100

    day 13 part 2

--- src/advent/day13.clj
+++ src/advent/day13.clj
@@ -1,5 +1,7 @@
 (ns advent.day13
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [advent.util :as util]))
 
 (def demo-input "939
 7,13,x,x,59,x,31,19")
@@ -17,5 +19,65 @@
 
 (def earliest-bus (apply min-key wait-time buses))
 
-(* earliest-bus (wait-time earliest-bus))
-;; => 3269
+(-> input
+    str/split-lines
+    second
+    (str/split #",")
+    (->> (keep-indexed
+          (fn [i v]
+            (when (not= v "x")
+              [i (read-string v)])))))
+
+
+;;; part 2, correct but too slow version
+;; {bus-id -> remainder}
+(def bus-departures
+  (-> input
+      str/split-lines
+      second
+      (str/split #",")
+      (->> (keep-indexed
+            (fn [i v]
+              (when (not= v "x")
+                (let [bus (read-string v)]
+                  [bus (mod (- bus i) bus)]))))
+           (into {}))))
+
+(def least-frequent-bus (apply max buses))
+
+(def other-departures (dissoc bus-departures least-frequent-bus))
+
+
+#_
+(loop [n (bus-departures least-frequent-bus)]
+  (if (every?
+       (fn [[bus remainder]]
+         (= (mod n bus) remainder))
+       other-departures)
+    n
+    (recur (+ n least-frequent-bus))))
+
+;; I had already seen this spoiler, and the fact that bus IDs are pairwise
+;; coprime:
+;;
+;; https://en.wikipedia.org/wiki/Chinese_remainder_theorem
+;;
+;; So I'm using this algorithm:
+;;
+;; https://brilliant.org/wiki/chinese-remainder-theorem/
+
+(def N (apply * buses))
+(def ys (map #(quot N %) buses))
+(def zs (map util/mul-inv ys buses))
+
+(mod
+ (apply
+  +
+  (map
+   (fn [n y z]
+     (* (bus-departures n) y z))
+   buses
+   ys
+   zs))
+ N)
+;; => 672754131923874
